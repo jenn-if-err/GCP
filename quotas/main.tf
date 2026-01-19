cda245d7170c7532217b5b706d2bae775ce7b6eb
Jennifer Tongco <jennifer.j.tongco@gmail.com>
2026-01-20 07:23:47 +0800

    Initialize main.tf for GCP quotas configuration

---
+++ quotas/main.tf
@@ -0,0 +1,26 @@
+variable "instance_count" {
+  default = 24
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = "asia-northeast1-c"
+  credentials = file("~/terraform-key.json")
+}
+
+resource "google_compute_instance" "vm_example" {
+  count        = var.instance_count
+  name         = "quota-vm-${count.index}"
+  machine_type = "e2-micro"
+  zone         = "asia-northeast1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
